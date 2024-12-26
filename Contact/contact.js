530bb8f7c97fa2bd8ed49e1314c5dcd5fe26413f
prigspagaloy <147347773+prigspagaloy@users.noreply.github.com>
2024-12-26 11:09:08 +0800

    ambot

---
+++ Contact/contact.js
@@ -0,0 +1,9 @@
+const navHam = document.getElementById("hamburger");
+const navMenu = document.getElementById("navbar");
+const navBtn = document.getElementById("menu");
+
+navHam.addEventListener("click", () => {
+    navHam.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    navBtn.classList.toggle("active");
+});
